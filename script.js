affae1efe8a7c77008d8a1f575f29e666f991ad4
negreaandrei <andrei@minimac2.skadool.private>
2015-10-16 20:51:53 +0300

    Updated script in order to work with new order history
    I’ve updated the script such that it will work ok also for the new
    order history implementation (currently under development, can be
    accessed using queryString=‘?newOrderHistory=true’). Also fixed bug to
    show correct total value of a diner with more than one selection.
    Thank you for implementing this. Keep up the good work.

--- script.js
+++ script.js
@@ -2,83 +2,110 @@
  * Section
  * ======================================================================== */
 
-var orders = [];
-
 document.addEventListener('DOMContentLoaded', function() {
 
-  // get orders from page
+      // get orders from page
 
-  chrome.tabs.executeScript(null, {
-    code: "\
-      var orders = [];\
-      if (document.querySelectorAll('h1')[0].innerText.toLowerCase().indexOf('istoric comenzi') != -1) {\
-        var tags = document.querySelectorAll('.history-item')[0].querySelectorAll('.content');\
-        for (var i = 0; i < tags.length; i++) {\
-          orders.push({\
-            name: tags[i].querySelector('h4').innerText.trim(),\
-            price: tags[i].querySelector('.price').innerText.trim(),\
-          });\
+    chrome.tabs.executeScript(null, {
+        code: "\
+      var orders = [], iterator, tags, dinerNameDOMElement, dinerName, priceValueDOMElement, priceValue;\
+      if (document.location.hash.startsWith('#history')) {\
+        tags = document.querySelectorAll('.history-item')[0].querySelectorAll('.content');\
+        if (tags.length == 0) {\
+        tags = document.querySelectorAll('.history-diners')[0].querySelectorAll('.content');\
+    }\
+\
+    for (iterator = 0; iterator < tags.length; iterator++) {\
+        dinerNameDOMElement = tags[iterator].querySelector('h4');\
+        dinerName = dinerNameDOMElement.innerText.trim();\
+\
+        priceValueDOMElement = tags[iterator].querySelector('.simple-footer');\
+        if (priceValueDOMElement != null) {\
+            priceValue = priceValueDOMElement.innerHTML.trim();\
+            priceValue = priceValue.replace('Total ' + dinerName + ': ', '');\
+        } else {\
+            priceValueDOMElement = tags[iterator].querySelector('footer').querySelectorAll('td');\
+            priceValueDOMElement = priceValueDOMElement[priceValueDOMElement.length - 1];\
+            priceValue = priceValueDOMElement.innerHTML.trim();\
         }\
-      } else {\
+        orders.push({\
+            name: dinerName,\
+            price: priceValue\
+        });\
+    }\
+    } else {\
         var name_tags = document.querySelectorAll('h1');\
         var price_tags = document.querySelectorAll('div.price');\
-        for (var i = 0; i < name_tags.length; i++) {\
-          orders.push({\
-            name: name_tags[i].innerText.trim(),\
-            price: price_tags[i].innerText.trim(),\
-          });\
+        for (iterator = 0; iterator < name_tags.length; iterator++) {\
+            orders.push({\
+                name: name_tags[iterator].innerText.trim(),\
+                price: price_tags[iterator].innerText.trim()\
+            });\
         }\
-      }\
-      JSON.stringify(orders);\
+    }\
+    JSON.stringify(orders);\
     "
-  }, function(orders) {
-    orders = JSON.parse(orders);
-    orders.sort(function(a, b) {
-      var x = a.name.toLowerCase();
-      var y = b.name.toLowerCase();
-      return ((x < y) ? -1 : ((x > y) ? 1 : 0));
-    });
+    }, function(orders) {
+        var html = '', iterator, iteratorChecks, checks, dinerName, pageURL, dinerKey;
 
-    // render the checklist
-
-    var html = '';
-    for (var i = 0; i < orders.length; i++) {
-      html = html + '<tr>';
-      html = html + '<td><label for="check' + i + '">' + orders[i].name + '</label></td>';
-      html = html + '<td><label for="check' + i + '">' + orders[i].price + '</label></td>';
-      html = html + '<td><input id="check' + i + '" type="checkbox" data-name="' + orders[i].name + '"' + (orders[i].payed ? ' checked ' : '') + '></td>';
-      html = html + '</tr>';
-    }
-    document.querySelector('tbody').innerHTML = html;
-    var checks = document.querySelectorAll('td input');
-    for (var i = 0; i < checks.length; i++) {
-      checks[i].addEventListener('click', function(event) {
-        var name = this.getAttribute('data-name');
-        for (var j = 0; j < orders.length; j++) {
-          if (orders[j].name == name) {
-            orders[j].payed = this.checked;
-            chrome.tabs.executeScript(null, {
-              code: "sessionStorage.setItem('" + name + "', " + this.checked + ")"
-            });
-          }
+        orders = JSON.parse(orders);
+        if (orders.length == 0) {
+            return;
         }
-      });
-    }
+        orders.sort(function(a, b) {
+            var x = a.name.toLowerCase();
+            var y = b.name.toLowerCase();
+            return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+        });
 
-    // get checkbox statuses from parent page
+        chrome.tabs.executeScript(null, {
+            code: "document.location.hash"
+        }, function(data) {
+            pageURL = data;
 
-    for (var i = 0; i < orders.length; i++) {
-      chrome.tabs.executeScript(null, {
-        code: i.toString() + " + ':' + sessionStorage.getItem('" + orders[i].name + "')"
-      }, function(data) {
-        data = data[0].split(':');
-        var index = parseInt(data[0]);
-        var value = (data[1] == 'true' ? true : false);
-        orders[index].payed = value;
-        document.querySelector('input[data-name="' + orders[index].name + '"]').checked = value;
-      });
-    }
-  });
+            // render the checklist
+            for (iterator = 0; iterator < orders.length; iterator++) {
+                html = html + '<tr>';
+                html = html + '<td><label for="check' + iterator + '">' + orders[iterator].name + '</label></td>';
+                html = html + '<td><label for="check' + iterator + '">' + orders[iterator].price + '</label></td>';
+                html = html + '<td><input id="check' + iterator + '" type="checkbox" data-name="' + orders[iterator].name + '"' + (orders[iterator].payed ? ' checked ' : '') + '></td>';
+                html = html + '</tr>';
+            }
+            document.querySelector('tbody').innerHTML = html;
+
+            checks = document.querySelectorAll('td input');
+            for (iteratorChecks = 0; iteratorChecks < checks.length; iteratorChecks++) {
+                checks[iteratorChecks].addEventListener('click', function() {
+                    dinerName = this.getAttribute('data-name');
+                    dinerKey = dinerName + ":url:" + pageURL;
+                    for (iterator = 0; iterator < orders.length; iterator++) {
+                        if (orders[iterator].name == dinerName) {
+                            orders[iterator].payed = this.checked;
+                            chrome.tabs.executeScript(null, {
+                                code: "sessionStorage.setItem('" + dinerKey + "', " + this.checked + ")"
+                            });
+                        }
+                    }
+                });
+            }
+
+            // get checkbox statuses from parent page
+
+            for (iterator = 0; iterator < orders.length; iterator++) {
+                dinerKey = orders[iterator].name + ":url:" + pageURL;
+
+                chrome.tabs.executeScript(null, {
+                    code: iterator.toString() + " + ':' + sessionStorage.getItem('" + dinerKey + "')"
+                }, function(data) {
+                    data = data[0].split(':');
+                    var index = parseInt(data[0]);
+                    var value = data[1] == 'true';
+                    orders[index].payed = value;
+                    document.querySelector('input[data-name="' + orders[index].name + '"]').checked = value;
+                });
+            }
+        });
+    });
 });
 
 /* ========================================================================
