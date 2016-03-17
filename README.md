e6d20058086890041513462d8a84b15d114a94ae
Gabi Nagy <gabrian.nagy@gmail.com>
2016-03-17 11:33:12 +0200

    Update stuff

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-hipmenu-checklist
-=================
+# hipmenu-checklist
 
-Turn a group order into an easy to manage checklist
+Turn a hipMenu group order into an easy to manage checklist
+
 
 ### What's this? ###
 
@@ -9,9 +9,11 @@ hipmenu-checklist is a Chrome extension that generates a checklist based on your
 
 ![hipmenu-checklist screenshot](https://raw.github.com/g4b1nagy/hipmenu-checklist/master/screenshot.png)
 
+
 ### How does it work? ###
 
 When clicked, the extension tries to figure out whether you're on an `order in progress` page or on the `order history` page. It then proceeds to extract every name - price pair that is finds on the page (it will limit itself to your latest order, if you're on the `order history` page) and generates a checklist based on those. Since browser action popups are not persistent, sessionStorage is used to keep track of the checkbox states.
+
 
 ### Feeling generous? ###
 
--- script.js
+++ script.js
@@ -1,104 +1,91 @@
-/* ========================================================================
- * Section
- * ======================================================================== */
-
 var orders = [];
 
 document.addEventListener('DOMContentLoaded', function() {
 
-  // get orders from page
+    // get orders from page
+    chrome.tabs.executeScript(null, {
+        code: "\
+            var orders = [];\
+            if (document.location.hash.indexOf('#history') == -1) {\
+                var my_name = document.querySelector('#h-profilename').textContent;\
+                var name_tags = document.querySelectorAll('h1');\
+                var order_tags = document.querySelectorAll('div.details');\
+                for (var i = 0; i < name_tags.length; i++) {\
+                    var tds = order_tags[i].querySelectorAll('footer td');\
+                    if (tds.length > 0) {\
+                        orders.push({\
+                            name: name_tags[i].textContent.replace('Selecțiile mele', my_name).trim(),\
+                            price: tds[tds.length - 1].textContent.trim(),\
+                        });\
+                    }\
+                }\
+            } else {\
+                var order_tags = document.querySelectorAll('.history-diners .container-marginTBMedium');\
+                for (var i = 0; i < order_tags.length; i++) {\
+                    var tds = order_tags[i].querySelectorAll('footer td');\
+                    orders.push({\
+                        name: order_tags[i].querySelector('h4').textContent.trim(),\
+                        price: tds[tds.length - 1].textContent.trim(),\
+                    });\
+                }\
+            }\
+            JSON.stringify(orders);\
+        "
+    }, function(orders) {
 
-  chrome.tabs.executeScript(null, {
-    code: "\
-      var orders = [];\
-      if (document.location.hash.indexOf('#history') == -1) {\
-        var my_name = document.querySelector('#h-profilename').textContent;\
-        var name_tags = document.querySelectorAll('h1');\
-        var order_tags = document.querySelectorAll('div.details');\
-        for (var i = 0; i < name_tags.length; i++) {\
-          var tds = order_tags[i].querySelectorAll('footer td');\
-          if (tds.length > 0) {\
-            orders.push({\
-              name: name_tags[i].textContent.replace('Selecțiile mele', my_name).trim(),\
-              price: tds[tds.length - 1].textContent.trim(),\
-            });\
-          }\
-        }\
-      } else {\
-        var order_tags = document.querySelectorAll('.history-diners .container-marginTBMedium');\
-        for (var i = 0; i < order_tags.length; i++) {\
-          var tds = order_tags[i].querySelectorAll('footer td');\
-          orders.push({\
-            name: order_tags[i].querySelector('h4').textContent.trim(),\
-            price: tds[tds.length - 1].textContent.trim(),\
-          });\
-        }\
-      }\
-      JSON.stringify(orders);\
-    "
-  }, function(orders) {
-
-    // build the orders object
-
-    orders = JSON.parse(orders);
-    orders.sort(function(a, b) {
-      var x = a.name.toLowerCase();
-      var y = b.name.toLowerCase();
-      return ((x < y) ? -1 : ((x > y) ? 1 : 0));
-    });
-
-    // render the checklist
-
-    var html = '';
-    for (var i = 0; i < orders.length; i++) {
-      html = html + '<tr>';
-      html = html + '<td><label for="check' + i + '">' + orders[i].name + '</label></td>';
-      html = html + '<td><label for="check' + i + '">' + orders[i].price + '</label></td>';
-      html = html + '<td><input id="check' + i + '" type="checkbox" data-name="' + orders[i].name + '"></td>';
-      html = html + '</tr>';
-    }
-    document.querySelector('tbody').innerHTML = html;
-
-    // add event listeners
-
-    var checks = document.querySelectorAll('td input');
-    for (var i = 0; i < checks.length; i++) {
-      checks[i].addEventListener('click', function(event) {
-        var name = this.getAttribute('data-name');
-        for (var j = 0; j < orders.length; j++) {
-          if (orders[j].name == name) {
-            chrome.tabs.executeScript(null, {
-              code: "sessionStorage.setItem('" + name + "', " + this.checked + ")"
-            });
-            syncState();
-          }
-        }
-      });
-    }
-
-    // get checkbox statuses from parent page
-
-    function syncState() {
-      var total = 0;
-      for (var i = 0; i < orders.length; i++) {
-        chrome.tabs.executeScript(null, {
-          code: i.toString() + " + ':' + sessionStorage.getItem('" + orders[i].name + "')"
-        }, function(data) {
-          data = data[0].split(':');
-          var index = parseInt(data[0]);
-          var value = (data[1] == 'true' ? true : false);
-          document.querySelector('input[data-name="' + orders[index].name + '"]').checked = value;
-          total = total + (value ? parseFloat(orders[index].price) : 0);
-          document.querySelector('tfoot').innerHTML = '<tr class="brand"><td>Total adunat:</td><td colspan="2">' + total.toFixed(2) + '</td></tr>';
+        // build the orders object
+        orders = JSON.parse(orders);
+        orders.sort(function(a, b) {
+            var x = a.name.toLowerCase();
+            var y = b.name.toLowerCase();
+            return ((x < y) ? -1 : ((x > y) ? 1 : 0));
         });
-      }
-    }
 
-    syncState();
+        // render the checklist
+        var html = '';
+        for (var i = 0; i < orders.length; i++) {
+            html = html + '<tr>';
+            html = html + '<td><label for="check' + i + '">' + orders[i].name + '</label></td>';
+            html = html + '<td><label for="check' + i + '">' + orders[i].price + '</label></td>';
+            html = html + '<td><input id="check' + i + '" type="checkbox" data-name="' + orders[i].name + '"></td>';
+            html = html + '</tr>';
+        }
+        document.querySelector('tbody').innerHTML = html;
 
-  });
+        // add event listeners
+        var checks = document.querySelectorAll('td input');
+        for (var i = 0; i < checks.length; i++) {
+            checks[i].addEventListener('click', function(event) {
+                var name = this.getAttribute('data-name');
+                for (var j = 0; j < orders.length; j++) {
+                    if (orders[j].name == name) {
+                        chrome.tabs.executeScript(null, {
+                            code: "sessionStorage.setItem('" + name + "', " + this.checked + ")"
+                        });
+                        syncState();
+                    }
+                }
+            });
+        }
+
+        // get checkbox statuses from parent page
+        function syncState() {
+            var total = 0;
+            for (var i = 0; i < orders.length; i++) {
+                chrome.tabs.executeScript(null, {
+                    code: i.toString() + " + ':' + sessionStorage.getItem('" + orders[i].name + "')"
+                }, function(data) {
+                    data = data[0].split(':');
+                    var index = parseInt(data[0]);
+                    var value = (data[1] == 'true' ? true : false);
+                    document.querySelector('input[data-name="' + orders[index].name + '"]').checked = value;
+                    total = total + (value ? parseFloat(orders[index].price) : 0);
+                    document.querySelector('tfoot').innerHTML = '<tr class="brand"><td>Total adunat:</td><td colspan="2">' + total.toFixed(2) + '</td></tr>';
+                });
+            }
+        }
+
+        syncState();
+
+    });
 });
-
-/* ========================================================================
- * Section
- * ======================================================================== */
